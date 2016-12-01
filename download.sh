d125a753cb905a1bffd4c04c605b3be0601885da
Jim Green <jimgreen2013@qq.com>
2016-12-01 22:24:58 +0800

    增加下载指定日期音频的功能

--- download.sh
+++ download.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 echo "start download merriam webster podcast: word of day"
 url="http://assets.merriam-webster.com/mw/podcasts/wod/"
-fileName="wd"$(date +%Y%m%d)".mp3"
+if [ $# == 1 ]
+then
+	fileName="wd"$1".mp3"
+else
+	fileName="wd"$(date +%Y%m%d)".mp3"
+fi
 url=${url}${fileName}
 filePath="./podcast/"${fileName}
 curl ${url} > ${filePath}
-echo "successfully download podcast"
+echo "successfully download podcast to "${filePath}
