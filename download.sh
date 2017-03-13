f64db9402fc36c6c418a7edd20717a3665dbd3ff
Jim Green <jimgreen2013@qq.com>
2017-03-13 23:43:10 +0800

    automatically download audio file in last 7 days

--- download.sh
+++ download.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 echo "start download merriam webster podcast: word of day"
-url="http://assets.merriam-webster.com/mw/podcasts/wod/"
-if [ $# == 1 ]
-then
-	fileName="wd"$1".mp3"
-else
-	fileName="wd"$(date +%Y%m%d)".mp3"
-fi
-url=${url}${fileName}
-filePath="./podcast/"${fileName}
-curl ${url} > ${filePath}
-echo "successfully download podcast to "${filePath}
+base_url="http://assets.merriam-webster.com/mw/podcasts/wod/"
+#if [ $# == 1 ]
+#then
+#	fileName="wd"$1".mp3"
+#else
+#	fileName="wd"$(date +%Y%m%d)".mp3"
+#	for ((i=1; i<=6; i++))
+#fi
+for ((i=0; i<7; i++));
+do
+	fileName="wd"$(date +%Y%m%d --date="${i} days ago")".mp3"
+	url=${base_url}${fileName}
+	filePath="./podcast/"${fileName}
+	if [ ! -f "${filePath}" ];
+	then
+		curl ${url} > ${filePath}
+		echo "successfully download podcast to "${filePath}
+	fi
+done
